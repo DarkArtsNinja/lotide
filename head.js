4d0c5c8b83e1c72d3d21ca400204e84d292e3e66
Dan Xue <danixue@gmail.com>
2021-09-13 22:07:34 +0000

    head.js function

--- head.js
+++ head.js
@@ -6,5 +6,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const head = function(arr){
+  if (arr.length === 0) {
+    return false;
+  } else {
+    return arr[0];
+  }
+}
+
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hsdfsdello");
