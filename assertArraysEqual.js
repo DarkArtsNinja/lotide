21c2d0e3cc05f396e8e81247996aa3189a049abb
Dan Xue <danixue@gmail.com>
2021-09-14 20:51:34 +0000

    compares both arrays and asserts if false/true

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+function assertArrayEqual(actual, expected){
+  let value;
+  for (let i = 0; i < actual.length ; i++) {
+    if (actual[i] === expected[i] && (i < actual.length - 1)) {
+      continue;
+    }
+    else if (actual[i] === expected[i] && (i === actual.length - 1)) {
+      value = true;
+    }
+    else {
+      value = false
+    }
+  }
+  if (value === true) {
+    return (`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return (`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+}
+
+console.log(assertArrayEqual([1, 2, 3], [1, 2, 3])) // => true
+console.log(assertArrayEqual([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(assertArrayEqual(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(assertArrayEqual(["1", "2", "3"], ["1", "2", 3])) // => false
+
+// assertEqual(([1, 2, 3], [1, 2, 3]), true); // => should PASS
