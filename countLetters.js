a58101b0a4baf74ef6e72a0f2d196499771a3288
Dan Xue <danixue@gmail.com>
2021-09-17 00:31:48 +0000

    more obejects stuff

--- countLetters.js
+++ countLetters.js
@@ -9,14 +9,21 @@ const assertEqual = function(actual, expected) {
 function countLetters(words){
   let result = {}; //store the values in here
 
-  for (const iterator of words) {
-    if (result[iterator]) {
-      result[iterator] += 1;
+  for (const letter of words) {
+
+    //how to get the index of an element in string of words
+    // console.log(words.indexOf(letter));
+    // console.log(letter);
+
+    if (result[letter]) {
+      //how to add an array to a value of an object
+      result[letter] += [letter];
     } else {
-      result[iterator] = 1 ;
+      console.log(result[letter] = letter) ;
     }
   }
   return result;
+  
 }
 
 console.log(countLetters("lighthouse in the house"));
