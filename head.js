04cdc579b603638caba0ac4c27bc421d5726f207
Dan Xue <danixue@gmail.com>
2021-09-20 21:55:56 +0000

    refactored to use module

--- head.js
+++ head.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const head = function(arr){
   if (arr.length === 0) {
@@ -13,6 +8,8 @@ const head = function(arr){
     return arr[0];
   }
 }
+
+module.exports = head; 
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
